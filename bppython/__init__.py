aac9bec27cf39ea8ce5020f3a9820b5a8c6f16c3
Ben Pritchard <ben@bennyp.org>
2015-06-24 15:53:43 -0400

    Clean up python stuff

--- bppython/__init__.py
+++ bppython/__init__.py
@@ -1,19 +1,65 @@
 import sys
 import os
 import importlib
-from . import loader
-from . import utils
+from .utils import *
 
-bpcore = None
-
-########################### 
-# Initialize the path, etc
-########################### 
-# store the path, etc. We will reset later
-modpath = "/home/ben/programming/BPModule/install/modules"
-sys.path.insert(0, modpath)
 sys.setdlopenflags(os.RTLD_NOW | os.RTLD_GLOBAL)
+import bpmodule_core as bpcore
 
-# Load the core module
-bpcore = loader.LoadCoreModule("bpmodule_core")
-print("Initialized core. bpcore = {}".format(bpcore))
+
+# Print info about the core module
+print(bpcore.minfo)
+for key,minfo in bpcore.minfo.items():
+  minfo["path"] = os.path.dirname(bpcore.__file__) + "/"
+  PrintModuleInfo(key, minfo)
+
+
+# Create the various stores and loaders
+mst = bpcore.ModuleStore()
+cml = bpcore.CModuleLoader(mst)
+pml = bpcore.PyModuleLoader(mst)
+
+
+
+
+def Init():
+    pass
+
+
+def Finalize():
+    pml.DeleteAll()
+    cml.DeleteAll()
+    cml.CloseHandles()
+
+
+def LoadModule(name):
+    olddl = sys.getdlopenflags()
+    sys.setdlopenflags(os.RTLD_NOW | os.RTLD_GLOBAL)
+    m = importlib.import_module(name)
+    sys.setdlopenflags(olddl)
+
+    path = os.path.dirname(m.__file__) + "/"
+
+    for key,minfo in m.minfo.items():
+        # set the path for all
+        minfo["path"] = path
+
+        # Dump some info
+        PrintModuleInfo(key, minfo)
+
+        # Load & insert
+        # skip core types and others
+        if minfo["type"] == "c_module":
+            cml.LoadSO(key, minfo)
+        elif minfo["type"] == "python_module":
+            pml.AddPyModule(key, m.CreateModule, minfo)
+
+    return m
+
+
+
+def LoadModules(names):
+    mods = []
+    for name in names:
+        mods.append(LoadModule(name))
+    return mods
--- test.py
+++ test.py
@@ -10,6 +10,7 @@ sys.path.insert(0, modpath)
 
 import bppython as bp
 
+bp.Init()
 
 # Set some outputs
 bp.bpcore.SetOut_Stdout()
@@ -17,19 +18,18 @@ bp.bpcore.SetOut_Stdout()
 bp.bpcore.SetColor(True)
 bp.bpcore.SetDebug(True)
 
-# Create the module store and loaders
-mst = bp.bpcore.ModuleStore()
-cml = bp.bpcore.CModuleLoader(mst)
-pml = bp.bpcore.PyModuleLoader(mst)
-
 # Load the python modules
-t1 = bp.loader.LoadCModule("testmodule1", cml)
-t2 = bp.loader.LoadPyModule("testpymodule1", pml)
+bp.LoadModules(
+               [
+                "testmodule1",
+                "testpymodule1"
+               ]
+              )
 
 # Test
 try:
-  b1 = mst.GetModule_Test("TESTMOD1")
-  b2 = mst.GetModule_Test("TESTPYMOD1")
+  b1 = bp.mst.GetModule_Test("TESTMOD1")
+  b2 = bp.mst.GetModule_Test("TESTPYMOD1")
 
   b1.RunTest()
   print()
@@ -51,8 +51,4 @@ except RuntimeError as e:
   print(e)
 
 
-pml.DeleteAll()
-cml.DeleteAll()
-cml.CloseHandles()
-
-
+bp.Finalize()
