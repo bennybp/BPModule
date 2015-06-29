e6b7e7343147acf132906c596019347260d27dfb
Ben Pritchard <ben@bennyp.org>
2015-06-29 12:46:52 -0400

    Pass through python exceptions

--- BPModule/core/ModuleInfo_python.cpp
+++ BPModule/core/ModuleInfo_python.cpp
@@ -11,28 +11,21 @@ namespace bpmodule {
 // the "options" element of the dict must be a list of tuples
 ModuleInfo::ModuleInfo(const bpy::dict & dictionary)
 {
-    try
-    {
-        key = bpy::extract<std::string>(dictionary["key"]);
-        name = bpy::extract<std::string>(dictionary["name"]);
-        type = bpy::extract<std::string>(dictionary["type"]);
-        path = bpy::extract<std::string>(dictionary["path"]);
-        version = bpy::extract<std::string>(dictionary["version"]);
-        description = bpy::extract<std::string>(dictionary["description"]);
-        authors = ConvertListToVec<std::string>(bpy::extract<bpy::list>(dictionary["authors"]));
-        refs = ConvertListToVec<std::string>(bpy::extract<bpy::list>(dictionary["refs"]));
+    key = bpy::extract<std::string>(dictionary["key"]);
+    name = bpy::extract<std::string>(dictionary["name"]);
+    type = bpy::extract<std::string>(dictionary["type"]);
+    path = bpy::extract<std::string>(dictionary["path"]);
+    version = bpy::extract<std::string>(dictionary["version"]);
+    description = bpy::extract<std::string>(dictionary["description"]);
+    authors = ConvertListToVec<std::string>(bpy::extract<bpy::list>(dictionary["authors"]));
+    refs = ConvertListToVec<std::string>(bpy::extract<bpy::list>(dictionary["refs"]));
 
-        OptionMap op;
-        bpy::list olist = bpy::extract<bpy::list>(dictionary["options"]);
-        options = OptionMap(olist);
+    OptionMap op;
+    bpy::list olist = bpy::extract<bpy::list>(dictionary["options"]);
+    options = OptionMap(olist);
 
-        if(dictionary.has_key("soname"))
-            soname = bpy::extract<std::string>(dictionary["soname"]);
-    }
-    catch(...)
-    {
-        out::Error("HEREEEE\n");
-    }
+    if(dictionary.has_key("soname"))
+        soname = bpy::extract<std::string>(dictionary["soname"]);
 }
 
 
