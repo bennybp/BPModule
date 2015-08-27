8be64c04cacf3b9e4a04d34b33415d9a68c35eec
Ben Pritchard <ben@bennyp.org>
2015-08-27 18:04:39 -0400

    Add MPI stuff

--- cmake/AddModule.cmake
+++ cmake/AddModule.cmake
@@ -12,6 +12,7 @@ macro(ADD_MODULE MODULE_NAME
       # CXX Flags
       list(APPEND ${MODULE_NAME}_CXX_FLAGS "${MODULE_CXX_FLAGS}")
       list(APPEND ${MODULE_NAME}_CXX_FLAGS "${OpenMP_CXX_FLAGS}")
+      list(APPEND ${MODULE_NAME}_CXX_FLAGS "${MPI_CXX_COMPILE_FLAGS}")
       list(APPEND ${MODULE_NAME}_CXX_FLAGS "-fPIC")
       list(APPEND ${MODULE_NAME}_CXX_FLAGS "-std=c++11;-Wall;-pedantic")
       string(REPLACE ";" " " ${MODULE_NAME}_CXX_FLAGS "${${MODULE_NAME}_CXX_FLAGS}")
@@ -21,14 +22,20 @@ macro(ADD_MODULE MODULE_NAME
       # Includes
       list(APPEND ${MODULE_NAME}_CXX_INCLUDES "${BPMODULE_PATH}")          # When building the core, in-source tree
       list(APPEND ${MODULE_NAME}_CXX_INCLUDES "${BPMODULE_PATH}/include")  # Out-of-source module build
+      list(APPEND ${MODULE_NAME}_CXX_INCLUDES "${MPI_CXX_INCLUDE_PATH}")
       list(APPEND ${MODULE_NAME}_CXX_INCLUDES "${BPMODULE_CYCLOPS_PATH}/include")
       list(APPEND ${MODULE_NAME}_CXX_INCLUDES "${MODULE_CXX_INCLUDES}")
       set_target_properties(${MODULE_NAME} PROPERTIES INCLUDE_DIRECTORIES "${${MODULE_NAME}_CXX_INCLUDES}")
       message(STATUS "${MODULE_NAME} cxx includes: ${${MODULE_NAME}_CXX_INCLUDES}")
 
       # Linker flags
+      message(STATUS "HERE: ${OpenMP_CXX_FLAGS}")
+      message(STATUS "HERE: ${MPI_CXX_LINK_FLAGS}")
+      message(STATUS "HERE: ${MPI_CXX_LIBRARIES}")
       list(APPEND ${MODULE_NAME}_CXX_LINK_FLAGS "${MODULE_CXX_LINK_FLAGS}")
       list(APPEND ${MODULE_NAME}_CXX_LINK_FLAGS "${OpenMP_CXX_FLAGS}")
+      list(APPEND ${MODULE_NAME}_CXX_LINK_FLAGS "${MPI_CXX_LINK_FLAGS}")
+      list(APPEND ${MODULE_NAME}_CXX_LINK_FLAGS "${MPI_CXX_LIBRARIES}")
       list(APPEND ${MODULE_NAME}_CXX_LINK_FLAGS "${BPMODULE_CYCLOPS_PATH}/lib/libctf.a")
       target_link_libraries(${MODULE_NAME} "${${MODULE_NAME}_CXX_LINK_FLAGS}")
       message(STATUS "${MODULE_NAME} cxx link flags: ${${MODULE_NAME}_CXX_LINK_FLAGS}")
--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -4,6 +4,14 @@ if(NOT OPENMP_FOUND)
   message(FATAL_ERROR "Error - OpenMP is required!")
 endif()
 
+find_package(MPI)
+if(NOT MPI_CXX_FOUND) 
+  message(FATAL_ERROR "Error - MPI is required!")
+endif()
+
+# For some reason, this has a leading whitespace
+string(STRIP "${MPI_CXX_LINK_FLAGS}" MPI_CXX_LINK_FLAGS)
+
 find_package(Boost COMPONENTS python3 REQUIRED)
 
 set(Python_ADDITIONAL_VERSIONS "3.3;3.4")
