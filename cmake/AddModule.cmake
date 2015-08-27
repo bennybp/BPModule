5e2bd142d34cb57082657c55c3fc70073f387d8d
Ben Pritchard <ben@bennyp.org>
2015-08-27 15:17:38 -0400

    Fully separate modules. Fix some cmake stuff

--- cmake/AddModule.cmake
+++ cmake/AddModule.cmake
@@ -5,30 +5,30 @@ macro(ADD_MODULE MODULE_NAME
                  MODULE_CXX_LINK_FLAGS
                  MODULE_CXX_INCLUDES)
 
-  set(CMAKE_SHARED_MODULE_PREFIX_OLD ${CMAKE_SHARED_MODULE_PREFIX})
-  set(CMAKE_SHARED_MODULE_PREFIX "")
-
   if(NOT "${MODULE_FILES}" STREQUAL "")
       message(STATUS "Adding module ${MODULE_NAME}")
       add_library(${MODULE_NAME} MODULE ${MODULE_FILES} )
 
       # CXX Flags
       list(APPEND ${MODULE_NAME}_CXX_FLAGS "${MODULE_CXX_FLAGS}")
+      list(APPEND ${MODULE_NAME}_CXX_FLAGS "-fPIC")
+      list(APPEND ${MODULE_NAME}_CXX_FLAGS "-std=c++11;-Wall;-pedantic")
       string(REPLACE ";" " " ${MODULE_NAME}_CXX_FLAGS "${${MODULE_NAME}_CXX_FLAGS}")
-      message(STATUS "${MODULE_NAME} cxx compile flags: ${${MODULE_NAME}_CXX_FLAGS}")
       set_target_properties(${MODULE_NAME} PROPERTIES COMPILE_FLAGS "${${MODULE_NAME}_CXX_FLAGS}")
+      message(STATUS "${MODULE_NAME} cxx compile flags: ${${MODULE_NAME}_CXX_FLAGS}")
 
       # Includes
+      list(APPEND ${MODULE_NAME}_CXX_INCLUDES "${BPMODULE_PATH}")          # When building the core, in-source tree
+      list(APPEND ${MODULE_NAME}_CXX_INCLUDES "${BPMODULE_PATH}/include")  # Out-of-source module build
       list(APPEND ${MODULE_NAME}_CXX_INCLUDES "${MODULE_CXX_INCLUDES}")
-      message(STATUS "${MODULE_NAME} cxx includes: ${${MODULE_NAME}_CXX_INCLUDES}")
       set_target_properties(${MODULE_NAME} PROPERTIES INCLUDE_DIRECTORIES "${${MODULE_NAME}_CXX_INCLUDES}")
+      message(STATUS "${MODULE_NAME} cxx includes: ${${MODULE_NAME}_CXX_INCLUDES}")
 
       # Linker flags
       list(APPEND ${MODULE_NAME}_CXX_LINK_FLAGS "${MODULE_CXX_LINK_FLAGS}")
-      message(STATUS "${MODULE_NAME} cxx link flags: ${${MODULE_NAME}_CXX_LINK_FLAGS}")
       target_link_libraries(${MODULE_NAME} "${${MODULE_NAME}_CXX_LINK_FLAGS}")
-  endif()
+      message(STATUS "${MODULE_NAME} cxx link flags: ${${MODULE_NAME}_CXX_LINK_FLAGS}")
 
-  set(CMAKE_SHARED_MODULE_PREFIX ${CMAKE_SHARED_MODULE_PREFIX_OLD})
+  endif()
 
 endmacro()
--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -12,3 +12,7 @@ find_package(Boost COMPONENTS python3 REQUIRED)
 
 set(Python_ADDITIONAL_VERSIONS "3.3;3.4")
 find_package(PythonLibs REQUIRED)
+  
+
+# Don't use anything (ie, lib) as a prefix to SO
+set(CMAKE_SHARED_MODULE_PREFIX "")
