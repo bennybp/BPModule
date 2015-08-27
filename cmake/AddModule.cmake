ef53474cbd73d8038ff1f8e64d1e51a6766f2073
Ben Pritchard <ben@bennyp.org>
2015-08-27 17:29:24 -0400

    Tentatively add cyclops stuff

--- cmake/AddModule.cmake
+++ cmake/AddModule.cmake
@@ -21,6 +21,7 @@ macro(ADD_MODULE MODULE_NAME
       # Includes
       list(APPEND ${MODULE_NAME}_CXX_INCLUDES "${BPMODULE_PATH}")          # When building the core, in-source tree
       list(APPEND ${MODULE_NAME}_CXX_INCLUDES "${BPMODULE_PATH}/include")  # Out-of-source module build
+      list(APPEND ${MODULE_NAME}_CXX_INCLUDES "${BPMODULE_CYCLOPS_PATH}/include")
       list(APPEND ${MODULE_NAME}_CXX_INCLUDES "${MODULE_CXX_INCLUDES}")
       set_target_properties(${MODULE_NAME} PROPERTIES INCLUDE_DIRECTORIES "${${MODULE_NAME}_CXX_INCLUDES}")
       message(STATUS "${MODULE_NAME} cxx includes: ${${MODULE_NAME}_CXX_INCLUDES}")
@@ -28,6 +29,7 @@ macro(ADD_MODULE MODULE_NAME
       # Linker flags
       list(APPEND ${MODULE_NAME}_CXX_LINK_FLAGS "${MODULE_CXX_LINK_FLAGS}")
       list(APPEND ${MODULE_NAME}_CXX_LINK_FLAGS "${OpenMP_CXX_FLAGS}")
+      list(APPEND ${MODULE_NAME}_CXX_LINK_FLAGS "${BPMODULE_CYCLOPS_PATH}/lib/libctf.a")
       target_link_libraries(${MODULE_NAME} "${${MODULE_NAME}_CXX_LINK_FLAGS}")
       message(STATUS "${MODULE_NAME} cxx link flags: ${${MODULE_NAME}_CXX_LINK_FLAGS}")
 
--- cmake/Cyclops.cmake
+++ cmake/Cyclops.cmake
@@ -1,6 +1,6 @@
 
-#set(BPMODULE_CYCLOPS_PATH "" CACHE PATH "Path to Cyclops")
-#if("${BPMODULE_CYCLOPS_PATH}" STREQUAL "")
-#  message(FATAL_ERROR "Cyclops path is required!")
-#endif() 
+set(BPMODULE_CYCLOPS_PATH "" CACHE PATH "Path to Cyclops")
+if("${BPMODULE_CYCLOPS_PATH}" STREQUAL "")
+  message(FATAL_ERROR "Cyclops path is required!")
+endif() 
 
