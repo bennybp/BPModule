1389d98dab2ce28030246f9819d0e7cc8994ebf6
Ben Pritchard <ben@bennyp.org>
2015-08-28 13:50:19 -0400

    Add start of tensor stuff

---
+++ BPModule/tensor/Tensor.hpp
@@ -0,0 +1,14 @@
+#ifndef BPMODULE_TENSOR_HPP
+#define BPMODULE_TENSOR_HPP
+
+#include <ctf.hpp>
+
+
+typedef CTF::Scalar<double, false> Scalar;
+typedef CTF::Vector<double, false> Vector;
+typedef CTF::Matrix<double, false> Matrix;
+typedef CTF::Tensor<double, false> Tensor;
+
+
+
+#endif
