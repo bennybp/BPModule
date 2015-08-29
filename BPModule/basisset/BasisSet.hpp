e1a638e02cd2b8241e0375514fc2a1cc14e42861
Ben Pritchard <ben@bennyp.org>
2015-08-28 21:50:40 -0400

    Add skeleton for molecule and basis set

---
+++ BPModule/basisset/BasisSet.hpp
@@ -0,0 +1,9 @@
+#ifndef BPMODULE_BASISSET_HPP
+#define BPMODULE_BASISSET_HPP
+
+namespace bpmodule {
+
+} // close namespace bpmodule
+
+
+#endif
---
+++ BPModule/molecule/Molecule.hpp
@@ -0,0 +1,9 @@
+#ifndef BPMODULE_MOLECULE_HPP
+#define BPMODULE_MOLECULE_HPP
+
+namespace bpmodule {
+
+} // close namespace bpmodule
+
+
+#endif
