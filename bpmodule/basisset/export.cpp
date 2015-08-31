1056308b0eb55b6f8f49431bbdedc99e40802a53
Ben Pritchard <ben@bennyp.org>
2015-08-30 21:16:09 -0400

    Add python stuff for molecule and basisset

---
+++ bpmodule/basisset/export.cpp
@@ -0,0 +1,22 @@
+// Parallelization
+#include "bpmodule/basisset/BasisSet.hpp"
+
+
+#include <boost/python.hpp>
+
+using namespace boost::python;
+
+
+namespace bpmodule {
+namespace basisset {
+namespace export_python {
+
+BOOST_PYTHON_MODULE(basisset)
+{
+}
+
+
+} // close namespace export_python
+} // close namespace basisset 
+} // close namespace bpmodule
+
---
+++ bpmodule/molecule/export.cpp
@@ -0,0 +1,22 @@
+// Parallelization
+#include "bpmodule/molecule/Molecule.hpp"
+
+
+#include <boost/python.hpp>
+
+using namespace boost::python;
+
+
+namespace bpmodule {
+namespace molecule {
+namespace export_python {
+
+BOOST_PYTHON_MODULE(molecule)
+{
+}
+
+
+} // close namespace export_python
+} // close namespace molecule 
+} // close namespace bpmodule
+
