a911e64e0b8800219d30685e8d99522b7915f689
Ben Pritchard <ben@bennyp.org>
2015-09-22 22:17:17 -0400

    Work on options test

--- test/ModuleOptions.py
+++ test/ModuleOptions.py
@@ -15,22 +15,11 @@ import bpmodule as bp
 def Run():
     try:
       # Load the python modules
-      #             supermodule      module name      key
-      bp.LoadModule("testmodule1",   "testmodule1",   "TESTMOD1")
-      bp.LoadModule("testpymodule1", "testpymodule1", "TESTPYMOD1")
+      #             supermodule       module name       key
+      bp.LoadModule("test_options",   "testoptions1",   "TESTOPT1")
 
-      bp.SetOptions("TESTMOD1", { "double_opt_def": 1.111, "int_opt_def" : 55 })
-      bp.DumpModuleInfo()
-      bp.SanityCheck()
-
-
-      b1 = bp.mst.GetModule_Test("TESTMOD1")
-      b2 = bp.mst.GetModule_Test("TESTPYMOD1")
-
-      b1.RunTest()
-      bp.output.Output("\n")
-      b2.RunTest()
-      bp.output.Output("\n")
+      b1 = bp.mst.GetModule_Test("TESTOPT1")
+      bpmoutput.Output("Got module: %1% v%2%\n", b1.Version(), b1.Name())
 
       bp.output.Output("\nDone testing\n")
 
--- test/OptionMap.py
+++ test/OptionMap.py
@@ -21,67 +21,119 @@ def Run():
     try:
         bp.testing.PrintHeader("Testing construction of OptionMap objects")
 
-        #                  key               type                default,               required, Validator,          valid?
-        testelements = [ ( "int_1",          "int",              int(5)                ,  False,  None,               True ),
-                         ( "int_2",          "int",              int(-5)               ,  False,  None,               True ),
-                         ( "float_1",        "float",            float(5.0)            ,  False,  None,               True ),
-                         ( "bool_1",         "bool",             True                  ,  False,  None,               True ),
-                         ( "bool_2",         "bool",             False                 ,  False,  None,               True ),
-                         ( "string_1",       "str",              "Hi"                  ,  False,  None,               True ),
-                         ( "string_2",       "str",              ""                    ,  False,  None,               True ),
-                         ( "dict_1",         "dict",             {"key" : int(5)}      ,  False,  None,               False),
-                         ( "tuple_1",        "tuple",            ("key", int(5))       ,  False,  None,               False),
-                         ( "set_1",          "set",              {"key", int(5)}       ,  False,  None,               False),
-                         ( "list_i",         "listint",          [ 5, 10, 15 ]         ,  False,  None,               True ),
-                         ( "list_f",         "listfloat",        [ 5.0, 10.0, 15.0 ]   ,  False,  None,               True ),
-                         ( "list_s",         "liststr",          [ "5", "10", "Hi" ]   ,  False,  None,               True ),
-                         ( "list_l",         "listlist",         [ [ 5, 10, 15 ] ]     ,  False,  None,               False),
-                         ( "list_d",         "listdict",         [ {"key" : 5 } ]      ,  False,  None,               False),
-                         ( "list_t",         "listtuple",        [ ("key" , 5 ) ]      ,  False,  None,               False),
-                         ( "list_set",       "listset",          [ {"key" , 5 } ]      ,  False,  None,               False),
-                         ( "list_0",         "listempty",        [  ]                  ,  False,  None,               False),
-                         ( "list_1",         "listhetero",       [ 5, 10.1 ]           ,  False,  None,               False),
-                         ( "list_2",         "listhetero",       [ 5, "Hi" ]           ,  False,  None,               False),
-                         ( "list_3",         "listhetero",       [ "Hi", 5 ]           ,  False,  None,               False),
+        validtypes = ["int", "float", "bool", "str", "listint", "listfloat", "listbool", "liststr" ]
+
+        #                  key               type                default,              
+        testelements = [ ( "int_1",          "int",              int(5)                  ),
+                         ( "int_2",          "int",              int(-5)                 ),
+                         ( "float_1",        "float",            float(5.0)              ),
+                         ( "bool_1",         "bool",             True                    ),
+                         ( "bool_2",         "bool",             False                   ),
+                         ( "string_1",       "str",              "Hi"                    ),
+                         ( "string_2",       "str",              ""                      ),
+                         ( "dict_1",         "dict",             {"key" : int(5)}        ),
+                         ( "tuple_1",        "tuple",            ("key", int(5))         ),
+                         ( "set_1",          "set",              {"key", int(5)}         ),
+                         ( "list_i",         "listint",          [ 5, 10, 15 ]           ),
+                         ( "list_f",         "listfloat",        [ 5.0, 10.0, 15.0 ]     ),
+                         ( "list_b",         "listbool",         [ False, False, True ]  ),
+                         ( "list_s",         "liststr",          [ "5", "10", "Hi" ]     ),
+                         ( "list_l",         "listlist",         [ [ 5, 10, 15 ] ]       ),
+                         ( "list_d",         "listdict",         [ {"key" : 5 } ]        ),
+                         ( "list_t",         "listtuple",        [ ("key" , 5 ) ]        ),
+                         ( "list_set",       "listset",          [ {"key" , 5 } ]        ),
+                         ( "list_0",         "listempty",        [  ]                    ),
+                         ( "list_1",         "listhetero",       [ 5, 10.1 ]             ),
+                         ( "list_2",         "listhetero",       [ 5, "Hi" ]             ),
+                         ( "list_3",         "listhetero",       [ "Hi", 5 ]             ),
                        ]
 
         nfailed = 0
         ntest = 1
 
 
-        # Test single elements
+        # Test single element, no validator, not required, with default
         for d1 in testelements:
             s = "Test construction with {}".format(d1[0])
-            expected = d1[5]
-            opt = { d1[0] : ( d1[1], d1[2], d1[3], d1[4], "(no help)" ) }
+            expected = (d1[1] in validtypes)
+            opt = { d1[0] : ( d1[1], d1[2], False, None, "(no help)" ) }
             nfailed += bp.testing.TestOptionMap(ntest, s, expected, opt)
             ntest += 1
 
-        # Test two elements
-        # Duplicate keys aren't tested since the python dict
-        # has unique keys
+
+        # Test two elements, no validator, not required, with default
         for d1 in testelements:
             for d2 in testelements:
                 s = "Test construction with {} + {}".format(d1[0], d2[0])
-                expected = (d1[5] and d2[5])
-                opt = { d1[0] : ( d1[1], d1[2], d1[3], d1[4], "(no help)" ),  
-                        d2[0] : ( d2[1], d2[2], d2[3], d2[4], "(no help)" ) }
+                expected = (d1[1] in validtypes) and (d2[1] in validtypes)
+                opt = { d1[0] : ( d1[1], d1[2], False, None, "(no help)" ),  
+                        d2[0] : ( d2[1], d2[2], False, None, "(no help)" ) }
                 nfailed += bp.testing.TestOptionMap(ntest, s, expected, opt)
                 ntest += 1
-      
-        # Test three elements
-        # Duplicate keys aren't tested since the python dict
-        # has unique keys
+
+
+        # Test single element, no validator, not required, no default
+        for d1 in testelements:
+            s = "Test construction with {}".format(d1[0])
+            expected = (d1[1] in validtypes)
+            opt = { d1[0] : ( d1[1], None, False, None, "(no help)" ) }
+            nfailed += bp.testing.TestOptionMap(ntest, s, expected, opt)
+            ntest += 1
+
+
+        # Test two elements, no validator, not required, no default
         for d1 in testelements:
             for d2 in testelements:
-                for d3 in testelements:
-                    s = "Test construction with {} + {}".format(d1[0], d2[0])
-                    expected = (d1[5] and d2[5] and d3[5])
-                    opt = { d1[0] : ( d1[1], d1[2], d1[3], d1[4], "(no help)" ),  
-                            d2[0] : ( d2[1], d2[2], d2[3], d2[4], "(no help)" ),
-                            d3[0] : ( d3[1], d3[2], d3[3], d2[4], "(no help)" ) }
-                    nfailed += bp.testing.TestOptionMap(ntest, s, expected, opt)
-                    ntest += 1
+                s = "Test construction with {} + {}".format(d1[0], d2[0])
+                expected = (d1[1] in validtypes) and (d2[1] in validtypes)
+                opt = { d1[0] : ( d1[1], None, False, None, "(no help)" ),  
+                        d2[0] : ( d2[1], None, False, None, "(no help)" ) }
+                nfailed += bp.testing.TestOptionMap(ntest, s, expected, opt)
+                ntest += 1
+
+
+      
+        # Test single element, no validator, required, but with default specified
+        # (should always be false)
+        for d1 in testelements:
+            s = "Test construction with {}".format(d1[0])
+            expected = False
+            opt = { d1[0] : ( d1[1], d1[2], True, None, "(no help)" ) }
+            nfailed += bp.testing.TestOptionMap(ntest, s, expected, opt)
+            ntest += 1
+
+
+        # Test two elements, no validator, required, but with default specified
+        # (should always be false)
+        for d1 in testelements:
+            for d2 in testelements:
+                s = "Test construction with {} + {}".format(d1[0], d2[0])
+                expected = False
+                opt = { d1[0] : ( d1[1], d1[2], True, None, "(no help)" ),  
+                        d2[0] : ( d2[1], d2[2], True, None, "(no help)" ) }
+                nfailed += bp.testing.TestOptionMap(ntest, s, expected, opt)
+                ntest += 1
+
+
+        # Test single element, no validator, required, no default specified
+        for d1 in testelements:
+            s = "Test construction with {}".format(d1[0])
+            expected = (d1[1] in validtypes)
+            opt = { d1[0] : ( d1[1], None, True, None, "(no help)" ) }
+            nfailed += bp.testing.TestOptionMap(ntest, s, expected, opt)
+            ntest += 1
+
+
+        # Test two elements, no validator, required, no default specified
+        for d1 in testelements:
+            for d2 in testelements:
+                s = "Test construction with {} + {}".format(d1[0], d2[0])
+                expected = (d1[1] in validtypes) and (d2[1] in validtypes)
+                opt = { d1[0] : ( d1[1], None, True, None, "(no help)" ),  
+                        d2[0] : ( d2[1], None, True, None, "(no help)" ) }
+                nfailed += bp.testing.TestOptionMap(ntest, s, expected, opt)
+                ntest += 1
+
 
         bp.testing.PrintResults(nfailed) 
 
