270e39451b6381845755d129d00ad4706b5516af
Ben Pritchard <ben@bennyp.org>
2015-10-03 16:38:47 -0400

    Fix some round trip conversion testing stuff

--- bpmodule/testing/TestConvert.hpp
+++ bpmodule/testing/TestConvert.hpp
@@ -19,6 +19,18 @@ namespace testing {
 
 
 
+/*! \brief An class not exported to python
+ *
+ * Used to test failure of ConvertToPy
+ */
+template<typename T>
+struct FailObject
+{
+    T t;
+};
+
+
+
 /*! \brief Testing of python-to-C++ conversions
  *
  * \tparam T C++ type to use
@@ -68,6 +80,24 @@ void PyCppPy(const boost::python::object & obj)
 
 
 
+/*! \brief A single Python-to-C++-to-python round trip conversion that fails
+ *
+ * This should fail due to the object being passwd to ConvertToPy not
+ * being exported to python
+ *
+ * \tparam T C++ type to use
+ *
+ * \param [in] obj Object to convert
+ */
+template<typename T>
+void PyCppPy_Fail(const boost::python::object & obj)
+{
+    T t = python_helper::ConvertToCpp<T>(obj);
+    FailObject<T> fo{t};
+    boost::python::object obj2 = python_helper::ConvertToPy<FailObject<T>>(fo);
+}
+
+
 /*! \brief Test Python-to-C++-to-python round trip conversion
  *
  * \tparam T C++ type to use
@@ -85,6 +115,22 @@ int TestPyCppPy(int itest, const std::string & desc, bool expected, const boost:
 }
 
 
+
+/*! \brief Test Python-to-C++-to-python round trip conversion that fails
+ *
+ * \tparam T C++ type to use
+ *
+ * \param [in] itest A test number
+ * \param [in] desc Some description
+ * \param [in] expected True if this is supoosed to succeed, false if it should
+ *                      throw an exception
+ * \param [in] obj Object to convert
+ */
+template<typename T>
+int TestPyCppPy_Fail(int itest, const std::string & desc, bool expected, const boost::python::object & obj)
+{
+    return TestFunc(itest, desc, expected, PyCppPy_Fail<T>, obj);
+}
 
 
 } // close namespace testing
--- bpmodule/testing/export.cpp
+++ bpmodule/testing/export.cpp
@@ -20,7 +20,7 @@ namespace export_python {
 
 BOOST_PYTHON_MODULE(testing)
 {
-    // From-python conversions
+    // Py-Cpp-Py round trip tests
     // Arithmetic types
     def("TestPyCppPy_sshort",            TestPyCppPy<signed short>);
     def("TestPyCppPy_ushort",            TestPyCppPy<unsigned short>);
@@ -30,11 +30,9 @@ BOOST_PYTHON_MODULE(testing)
     def("TestPyCppPy_ulong",             TestPyCppPy<unsigned long>);
     def("TestPyCppPy_slonglong",         TestPyCppPy<signed long long>);
     def("TestPyCppPy_ulonglong",         TestPyCppPy<unsigned long long>);
-
     def("TestPyCppPy_float",             TestPyCppPy<float>);
     def("TestPyCppPy_double",            TestPyCppPy<double>);
     def("TestPyCppPy_longdouble",        TestPyCppPy<long double>);
-
     def("TestPyCppPy_string",            TestPyCppPy<std::string>);
 
     // Vector types
@@ -46,12 +44,49 @@ BOOST_PYTHON_MODULE(testing)
     def("TestPyCppPy_vector_ulong",      TestPyCppPy<std::vector<unsigned long>>);
     def("TestPyCppPy_vector_slonglong",  TestPyCppPy<std::vector<signed long long>>);
     def("TestPyCppPy_vector_ulonglong",  TestPyCppPy<std::vector<unsigned long long>>);
-
     def("TestPyCppPy_vector_float",      TestPyCppPy<std::vector<float>>);
     def("TestPyCppPy_vector_double",     TestPyCppPy<std::vector<double>>);
     def("TestPyCppPy_vector_longdouble", TestPyCppPy<std::vector<long double>>);
-
     def("TestPyCppPy_vector_string",     TestPyCppPy<std::vector<std::string>>);
+
+
+
+    // Py-Cpp-Py round trip tests that should fail on the
+    // cpp-to-py step
+    // Arithmetic types
+    def("TestPyCppPy_Fail_sshort",            TestPyCppPy_Fail<signed short>);
+    def("TestPyCppPy_Fail_ushort",            TestPyCppPy_Fail<unsigned short>);
+    def("TestPyCppPy_Fail_sint",              TestPyCppPy_Fail<signed int>);
+    def("TestPyCppPy_Fail_uint",              TestPyCppPy_Fail<unsigned int>);
+    def("TestPyCppPy_Fail_slong",             TestPyCppPy_Fail<signed long>);
+    def("TestPyCppPy_Fail_ulong",             TestPyCppPy_Fail<unsigned long>);
+    def("TestPyCppPy_Fail_slonglong",         TestPyCppPy_Fail<signed long long>);
+    def("TestPyCppPy_Fail_ulonglong",         TestPyCppPy_Fail<unsigned long long>);
+    def("TestPyCppPy_Fail_float",             TestPyCppPy_Fail<float>);
+    def("TestPyCppPy_Fail_double",            TestPyCppPy_Fail<double>);
+    def("TestPyCppPy_Fail_longdouble",        TestPyCppPy_Fail<long double>);
+    def("TestPyCppPy_Fail_string",            TestPyCppPy_Fail<std::string>);
+
+    // Vector types
+    def("TestPyCppPy_Fail_vector_sshort",     TestPyCppPy_Fail<std::vector<signed short>>);
+    def("TestPyCppPy_Fail_vector_ushort",     TestPyCppPy_Fail<std::vector<unsigned short>>);
+    def("TestPyCppPy_Fail_vector_sint",       TestPyCppPy_Fail<std::vector<signed int>>);
+    def("TestPyCppPy_Fail_vector_uint",       TestPyCppPy_Fail<std::vector<unsigned int>>);
+    def("TestPyCppPy_Fail_vector_slong",      TestPyCppPy_Fail<std::vector<signed long>>);
+    def("TestPyCppPy_Fail_vector_ulong",      TestPyCppPy_Fail<std::vector<unsigned long>>);
+    def("TestPyCppPy_Fail_vector_slonglong",  TestPyCppPy_Fail<std::vector<signed long long>>);
+    def("TestPyCppPy_Fail_vector_ulonglong",  TestPyCppPy_Fail<std::vector<unsigned long long>>);
+    def("TestPyCppPy_Fail_vector_float",      TestPyCppPy_Fail<std::vector<float>>);
+    def("TestPyCppPy_Fail_vector_double",     TestPyCppPy_Fail<std::vector<double>>);
+    def("TestPyCppPy_Fail_vector_longdouble", TestPyCppPy_Fail<std::vector<long double>>);
+    def("TestPyCppPy_Fail_vector_string",     TestPyCppPy_Fail<std::vector<std::string>>);
+
+
+
+
+
+
+
 
 
 
