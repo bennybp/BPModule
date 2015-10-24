8282f8390d44da69cefdfac6ff46563ccad69dcd
Ben Pritchard <ben@bennyp.org>
2015-10-24 09:40:36 -0400

    Add string joining

---
+++ bpmodule/output/FormatStr.cpp
@@ -0,0 +1,33 @@
+/*! \file
+ *
+ * \brief Various string formatting functions (source)
+ * \author Benjamin Pritchard (ben@bennyp.org)
+ */ 
+
+
+#include "bpmodule/output/FormatStr.hpp"
+
+
+/*! \brief Joins each element of a vector of strings
+ *
+ * \param [in] vec A vector of strings to join
+ * \param [in] j   What to put in between each string
+ * \return A string with each element of \p vec joined with \p j in between
+ */
+std::string Join(const std::vector<std::string> & vec, const std::string & j)
+{
+    if(vec.size() == 0)
+        return std::string();
+
+    std::string str = vec[0];
+
+    for(size_t i = 1; i < vec.size(); ++i)
+    {
+        str.append(j);
+        str.append(vec[i]);
+    }
+
+    return str;
+
+}
+
--- bpmodule/output/FormatStr.hpp
+++ bpmodule/output/FormatStr.hpp
@@ -1,12 +1,14 @@
 /*! \file
  *
- * \brief Formatting a string with boost::format
+ * \brief Various string formatting functions (header)
  * \author Benjamin Pritchard (ben@bennyp.org)
  */ 
 
 
 #ifndef _GUARD_FORMATSTR_HPP_
 #define _GUARD_FORMATSTR_HPP_
+
+#include <vector>
 
 #include "bpmodule/output/Output.hpp"
 
@@ -28,5 +30,16 @@ std::string FormatStr(const std::string & fmt, Targs... Fargs)
     bpmodule::output::Output(ss, fmt, Fargs...);
     return ss.str();
 }
+
+
+
+/*! \brief Joins each element of a vector of strings
+ *
+ * \param [in] vec A vector of strings to join
+ * \param [in] j   What to put in between each string
+ * \return A string with each element of \p vec joined with \p j in between
+ */
+std::string Join(const std::vector<std::string> & vec, const std::string & j);
+
 
 #endif
