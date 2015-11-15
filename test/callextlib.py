80e0682d42627a156dba949fed845c8a0b1c9b67
Ben Pritchard <ben@bennyp.org>
2015-11-15 18:13:09 -0500

    Add testing of external library linking with modules

---
+++ test/callextlib.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+
+import os
+import sys
+import argparse
+import traceback 
+
+sys.path.insert(0, "/home/ben/programming/BPModule/install/modules")
+
+import bpmodule as bp
+
+
+def Run(mm):
+    try:
+
+      # Load the python modules
+      #             supermodule      module name      key
+      mm.LoadModule("testextlib",   "testextlib",   "TESTEXTLIB")
+      mm.PrintInfo()
+      mm.SanityCheck()
+
+      b1 = mm.GetModule_Test("TESTEXTLIB")
+
+      b1.RunTest()
+      bp.output.Output("\n")
+
+      bp.output.Output("\nDone testing\n")
+
+    except Exception as e:
+      bp.output.Output("Caught exception in main handler\n")
+      traceback.print_exc()
+      bp.output.Error("\n")
+      bp.output.Error(str(e))
+      bp.output.Error("\n")
+
+
+
+bp.Init(sys.argv, out = "stdout", color = True, debug = True)
+
+with bp.ModuleManager() as mm:
+    Run(mm)
+
+bp.Finalize()
