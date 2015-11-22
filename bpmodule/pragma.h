9abd8f1bca252d95be9e128207ce9e39f2c6d747
Ben Pritchard <ben@bennyp.org>
2015-11-22 11:44:49 -0500

    Add intel pragmas to disable remarks

--- bpmodule/pragma.h
+++ bpmodule/pragma.h
@@ -13,15 +13,16 @@ extern "C" {
     #define PRAGMA_WARNING_POP                                 _Pragma("warning(pop)")
     #define PRAGMA_WARNING_PUSH                                _Pragma("warning(push)")
     #define PRAGMA_WARNING_IGNORE_UNUSED_PARAMETERS            _Pragma("warning(disable:869)")
-    #define PRAGMA_WARNING_IGNORE_UNUSED_VARIABLES //! \todo make this pragma
+    #define PRAGMA_WARNING_IGNORE_UNUSED_VARIABLES             _Pragma("warning(disable:177)")
     #define PRAGMA_WARNING_IGNORE_FP_EQUALITY                  _Pragma("warning(disable:1572)")
-    #define PRAGMA_WARNING_IGNORE_FP_CONVERT                   _Pragma("warning(disable:264)") 
-    #define PRAGMA_WARNING_IGNORE_CONVERT //! \todo Make this pragma if it exists
+    #define PRAGMA_WARNING_IGNORE_FP_CONVERT                   _Pragma("warning(disable:264)")
+    #define PRAGMA_WARNING_IGNORE_CONVERT                      _Pragma("warning(disable:2259)")
     #define PRAGMA_WARNING_IGNORE_SWITCH_MISSING_DEFAULT       _Pragma("warning(disable:2338)")
     #define PRAGMA_WARNING_IGNORE_POINTLESS_COMPARISON_UINT_0  _Pragma("warning(disable:186)")
     #define PRAGMA_WARNING_IGNORE_STATEMENT_UNREACHABLE        _Pragma("warning(disable:111)")
-    #define PRAGMA_WARNING_IGNORE_SHADOW   //! \todo Make this pragma
-    #define PRAGMA_WARNING_IGNORE_EXTRA_SEMICOLON //! \todo Make this pragma if it exists
+    #define PRAGMA_WARNING_IGNORE_SHADOW                       _Pragma("warning(disable:1599)")
+    #define PRAGMA_WARNING_IGNORE_EXTRA_SEMICOLON              // does not have warning for intel
+    #define PRAGMA_WARNING_IGNORE_REDECLARED_INLINE            _Pragma("warning(disable:522)")
 
 #elif defined(__GNUC__) || defined(__GNUG__)
 
@@ -30,16 +31,15 @@ extern "C" {
     #define PRAGMA_WARNING_POP                                 _Pragma("GCC diagnostic pop")
     #define PRAGMA_WARNING_IGNORE_UNUSED_PARAMETERS            _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")
     #define PRAGMA_WARNING_IGNORE_UNUSED_VARIABLES             _Pragma("GCC diagnostic ignored \"-Wunused-variable\"")
-    #define PRAGMA_WARNING_IGNORE_FP_EQUALITY                  _Pragma("GCC diagnostic ignored \"-Wfloat-equal\"") 
-    #define PRAGMA_WARNING_IGNORE_FP_CONVERT                   _Pragma("GCC diagnostic ignored \"-Wfloat-conversion\"") 
-    #define PRAGMA_WARNING_IGNORE_CONVERT                      _Pragma("GCC diagnostic ignored \"-Wconversion\"") 
+    #define PRAGMA_WARNING_IGNORE_FP_EQUALITY                  _Pragma("GCC diagnostic ignored \"-Wfloat-equal\"")
+    #define PRAGMA_WARNING_IGNORE_FP_CONVERT                   _Pragma("GCC diagnostic ignored \"-Wfloat-conversion\"")
+    #define PRAGMA_WARNING_IGNORE_CONVERT                      _Pragma("GCC diagnostic ignored \"-Wconversion\"")
     #define PRAGMA_WARNING_IGNORE_SWITCH_MISSING_DEFAULT       _Pragma("GCC diagnostic ignored \"-Wswitch-default\"")
     #define PRAGMA_WARNING_IGNORE_POINTLESS_COMPARISON_UINT_0  _Pragma("GCC diagnostic ignored \"-Wtype-limits\"")
-    #define PRAGMA_WARNING_IGNORE_EXTRA_SEMICOLON              _Pragma("GCC diagnostic ignored \"-Wpedantic\"")
+    #define PRAGMA_WARNING_IGNORE_STATEMENT_UNREACHABLE        //! \todo Is this a warning in GCC?
     #define PRAGMA_WARNING_IGNORE_SHADOW                       _Pragma("GCC diagnostic ignored \"-Wshadow\"")
-
-    //! \todo find these?
-    #define PRAGMA_WARNING_IGNORE_STATEMENT_UNREACHABLE       
+    #define PRAGMA_WARNING_IGNORE_EXTRA_SEMICOLON              _Pragma("GCC diagnostic ignored \"-Wpedantic\"")
+    #define PRAGMA_WARNING_IGNORE_REDECLARED_INLINE            // does not have warning for GCC
 
 #endif
 
