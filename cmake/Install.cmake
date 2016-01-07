16c990f57566eb856e7abb365aa27b57dcbe52bd
Ben Pritchard <ben@bennyp.org>
2016-01-06 22:43:50 -0500

    Clean up cmake - fixes graph install issues. Bonus: Remove header lists in cmake files

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -1,44 +1,27 @@
-macro(INSTALL_COREPYMODULE MODULE_NAME
-                       ADD_PYTHON_FILES)
-
-  list(APPEND ${MODULE_NAME}_PYFILES "__init__.py") 
-  list(APPEND ${MODULE_NAME}_PYFILES "modinfo.py") 
-  list(APPEND ${MODULE_NAME}_PYFILES "${ADD_PYTHON_FILES}") 
-  install(FILES ${${MODULE_NAME}_PYFILES} DESTINATION modules/bpmodule/${MODULE_NAME}) 
-endmacro()
-
-
-macro(INSTALL_COREMODULE MODULE_NAME
-                     HEADER_FILES
-                     ADD_PYTHON_FILES)
-
-  INSTALL_COREPYMODULE(${MODULE_NAME} "${ADD_PYTHON_FILES}")
+macro(INSTALL_COREMODULE MODULE_NAME)
 
   install(TARGETS ${MODULE_NAME} LIBRARY DESTINATION modules/bpmodule/${MODULE_NAME}
                                  RUNTIME DESTINATION modules/bpmodule/${MODULE_NAME})
 
-  install(FILES ${HEADER_FILES} DESTINATION include/bpmodule/${MODULE_NAME})
+  install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/" DESTINATION modules/bpmodule/${MODULE_NAME} FILES_MATCHING PATTERN "*.py")
+
+  # Headers for external modules to build against
+  install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/" DESTINATION include/bpmodule/${MODULE_NAME} FILES_MATCHING PATTERN "*.hpp")
+  install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/" DESTINATION include/bpmodule/${MODULE_NAME} FILES_MATCHING PATTERN "*.h")
+
 endmacro()
 
 
 
-macro(INSTALL_PYSUPERMODULE MODULE_NAME
-                       ADD_PYTHON_FILES)
+macro(INSTALL_SUPERMODULE MODULE_NAME ADD_FILES)
 
-  list(APPEND ${MODULE_NAME}_PYFILES "__init__.py") 
-  list(APPEND ${MODULE_NAME}_PYFILES "modinfo.py") 
-  list(APPEND ${MODULE_NAME}_PYFILES "${ADD_PYTHON_FILES}") 
-  install(FILES ${${MODULE_NAME}_PYFILES} DESTINATION modules/${MODULE_NAME}) 
-endmacro()
-
-
-macro(INSTALL_SUPERMODULE MODULE_NAME
-                          ADD_PYTHON_FILES)
-
-  INSTALL_PYSUPERMODULE(${MODULE_NAME} "${ADD_PYTHON_FILES}")
 
   install(TARGETS ${MODULE_NAME} LIBRARY DESTINATION modules/${MODULE_NAME}
                                  RUNTIME DESTINATION modules/${MODULE_NAME})
+
+  install(FILES "__init__.py"  DESTINATION modules/${MODULE_NAME})
+  install(FILES "modinfo.py"   DESTINATION modules/${MODULE_NAME})
+  install(FILES ${ADD_FILES}   DESTINATION modules/${MODULE_NAME})
 
   # Use this variable to get to your module's install path
   set(${MODULE_NAME}_INSTALL_PATH ${CMAKE_INSTALL_PREFIX}/modules/${MODULE_NAME})
