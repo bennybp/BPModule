242090fc1c193ed857479c5324c0fd7cac09044d
Ben Pritchard <ben@bennyp.org>
2016-01-13 12:36:56 -0500

    Move python detection to top level

--- bpmodule/cmake/Python.cmake
+++
@@ -1,9 +0,0 @@
-############################
-# Find Python libraries
-############################
-set(Python_ADDITIONAL_VERSIONS "3.3;3.4;3.5;3.6")
-find_package(PythonLibs REQUIRED)
-string(REGEX REPLACE "^([0-9]+)\\.([0-9]+)\\.[0-9]+.*"
-                     "\\1.\\2"
-                     PYTHONLIBS_MAJORMINOR_VERSION_STRING
-                     "${PYTHONLIBS_VERSION_STRING}")
