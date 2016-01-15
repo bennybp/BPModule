426df1d9445dbf1ec0d2b49167d797a88b63fd2a
Ben Pritchard <ben@bennyp.org>
2016-01-15 12:28:10 -0500

    Add MADNESS support to bpmodule::parallel

--- bpmodule/__init__.py
+++ bpmodule/__init__.py
@@ -65,7 +65,7 @@ def Init(argv, out = "stdout", color = True, debug = False):
   util.SetCmdline(argv)
 
   # Initialize Parallel
-  parallel.InitParallel()
+  parallel.Init()
 
   # Initialize tensor
   tensor.Init()
@@ -86,7 +86,7 @@ def Finalize():
   tensor.Finalize()
 
   output.Output("Finalizing parallelization\n")
-  parallel.FinalizeParallel()
+  parallel.Finalize()
 
   util.ClearCmdline()
 
