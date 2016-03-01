6709e40a4c3770c5245add4c38815bdf75b2efa6
Ben Pritchard <ben@bennyp.org>
2016-02-29 22:53:24 -0500

    Small cleanup of ModuleAdministrator

--- bpmodule/ModuleAdministrator.py
+++ bpmodule/ModuleAdministrator.py
@@ -5,14 +5,12 @@ import importlib
 from . import modulemanager, output, exception, datastore, CheckSupermodule
 
 
-
+# TODO - document me
 class ModuleAdministrator(modulemanager.ModuleManager):
     def __init__(self):
         super(ModuleAdministrator, self).__init__()
 
-        # TODO - remove me?
-        #self.modmap = {}
-
+        # Module search paths
         self.paths = [ ]
 
 
@@ -40,17 +38,19 @@ class ModuleAdministrator(modulemanager.ModuleManager):
             # Don't use RTLD_GLOBAL from now on
             olddl = sys.getdlopenflags()
             sys.setdlopenflags(os.RTLD_NOW)
-            m = importlib.import_module(supermodule)
-            sys.setdlopenflags(olddl)
 
-            # reset the paths
+            # Actually importing here
+            m = importlib.import_module(supermodule)
+
+            # reset the flags and paths
+            sys.setdlopenflags(olddl)
             sys.path = oldpath
 
             # get full path of loaded supermodule
             spath = os.path.dirname(m.__file__)
 
             # check options, etc
-            #CheckSupermodule(spath)
+            CheckSupermodule(spath)
 
         except exception.GeneralException as e:
             raise e
@@ -60,42 +60,44 @@ class ModuleAdministrator(modulemanager.ModuleManager):
                                              "supermodule", supermodule,
                                              "exception", str(e)) from None
 
+        # Check to see if this supermodule actually contains
+        # the desired module
         if not modulename in m.minfo:
             raise exception.GeneralException("Supermodule doesn't have module!",
                                              "supermodule", supermodule,
                                              "modulename", modulename)
 
 
+        # Extract the module info for the desired module
         minfo = m.minfo[modulename]
-
-        path = os.path.dirname(m.__file__)
 
         output.Output("\n")
         output.Output("Loading module %1% v%2%\n", modulename, minfo["version"])
 
-        # Create a c++ moduleinfo
+        # Create a c++ moduleinfo structure
+        # And fill it
         cppminfo = modulemanager.ModuleInfo()
         cppminfo.name = modulename
-        cppminfo.path = path
-
-        if "modpath" in minfo:
-            cppminfo.path = os.path.join(cppminfo.path, minfo["modpath"])
-
+        cppminfo.path = spath
         cppminfo.type = minfo["type"]
-
         cppminfo.version = minfo["version"]
         cppminfo.description = minfo["description"]
         cppminfo.authors = minfo["authors"]
         cppminfo.refs = minfo["refs"]
 
+        # Modpath is used for C++ SO modules
+        # if it exists, append it
+        if "modpath" in minfo:
+            cppminfo.path = os.path.join(cppminfo.path, minfo["modpath"])
 
-        # Create the options
+
+        # Create the options object
         for optkey,opt in minfo["options"].items():
             oph = datastore.MakeOptionHolder(optkey, opt[0], opt[2], opt[3], opt[4], opt[1])
             cppminfo.options.AddOption(oph)
         
 
-        # actually load
+        # Now we can actually load it
         super(ModuleAdministrator, self).LoadModuleFromModuleInfo(cppminfo)
 
         output.Debug("Imported module name %1% from %2%\n", modulename, supermodule)
@@ -105,11 +107,7 @@ class ModuleAdministrator(modulemanager.ModuleManager):
         output.Debug("Associated key %1% with module %2%\n", modulekey, supermodule)
         output.Output("\n")
 
-        # TODO - remove me?
-        #self.modmap[modulekey] = minfo;
-
-
 
     def SanityCheck(self):
-        # May add more here
+        # TODO May add more here
         super(ModuleAdministrator, self).TestAll() 
