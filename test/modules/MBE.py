df46beea0e3aa843c87d3b22245412713472bba7
Ryan Richard <ryanmrichard1@gmail.com>
2016-03-30 19:29:00 -0400

    Made MBE.py a test

--- test/modules/MBE.py
+++ test/modules/MBE.py
@@ -15,13 +15,37 @@ import bpmodule as bp
 def ApplyBasis(syst,bsname,bslabel="primary",bstype=bp.system.ShellType.Gaussian):
     return bp.system.ApplySingleBasis(bstype,bslabel,bsname,syst)
 
+def CompareEgy(EgyIn):
+   return EgyIn+224.90851104986243<0.00001
+
+def CompareGrad(GradIn):
+   CorrectGrad=[0.0056338166169999995, 0.0036814809890000004, 0.052711233995, 
+                0.019826950933, -0.0014316311719999998, -0.031158701642999997,
+                -0.024939313557, -0.0048274306840000005, -0.024943585464,
+                -0.018262424115999997, -0.047669106939, -0.006046442297,
+                 0.015971721606, 0.024963209265, -0.019093643937999998, 
+                 0.00619578613, 0.025219535787000005, 0.027208442172, 
+                 0.017581587257, 0.036519045374999996, -0.035085608934999984,
+                -0.028617330772000002, -0.023986983217, 0.003537758562,
+               0.006609205900000001, -0.012468119404999999, 0.03287054754200001]
+   AllGood=True
+   for i in range(0,27):
+        AllGood=AllGood and CorrectGrad[i]-GradIn[i]<0.00001
+   return AllGood
+
 def Run(mm):
     try:
+        tester = bp.testing.Tester("Testing MBE via MIM")
+        tester.PrintHeader()
+
         mm.LoadModule("Methods","MBE","MBE")
         mm.LoadModule("Methods","MIM","MIM")
         mm.LoadModule("Methods","SCF","SCF")
         mm.LoadModule("SystemFragmenters","Bondizer","FRAG")
-
+        
+        mm.ChangeOption("MBE","METHOD","SCF")
+        mm.ChangeOption("FRAG","TRUNCATION_ORDER",2)       
+ 
         MyMod=mm.GetModule("MBE",0)
         mol=bp.system.MakeSystem("""
         0 1
@@ -39,9 +63,20 @@ def Run(mm):
         wfn=bp.datastore.Wavefunction()
         wfn.system=mol
         MyMod.SetWfn(wfn)
-        MyMod.Options().Change("METHOD","SCF")
-        print(MyMod.Deriv(0))
-      
+        
+
+        Egy=MyMod.Deriv(0)
+        tester.Test("Testing Energy via Deriv(0)", True, CompareEgy, Egy[0])
+        Egy=MyMod.Energy()
+        tester.Test("Testing Energy via Energy()", True, CompareEgy, Egy)
+        Grad=MyMod.Deriv(1)
+        tester.Test("Testing Gradient via Deriv(1)", True, CompareGrad, Grad)
+        Grad=MyMod.Gradient()
+        tester.Test("Testing Gradient via Gradient()", True, CompareGrad, Grad)
+
+        tester.PrintResults()
+        
+     
     except Exception as e:
       bp.output.Output("Caught exception in main handler\n")
       traceback.print_exc()
