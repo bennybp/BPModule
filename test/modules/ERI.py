ef99ea6348969f9b62fd7a798489787c0778e74f
Ben Pritchard <ben@bennyp.org>
2016-04-10 18:00:11 -0400

    Update ERI test

--- test/modules/ERI.py
+++ test/modules/ERI.py
@@ -3,20 +3,25 @@
 
 import os
 import sys
-import argparse
 import traceback 
 
 # Add the bpmodule path
 thispath = os.path.dirname(os.path.realpath(__file__))
 bppath = os.path.join(os.path.dirname(thispath), "../", "modules")
+parent = os.path.join(os.path.dirname(thispath))
 sys.path.insert(0, bppath)
+sys.path.insert(0, parent)
 
-import bpmodule as bp
+from bpmodule.system import *
+from bpmodule.datastore import *
+from bpmodule.output import *
+
+from helper.TestAtoms import water
 
 
 def Run(mm):
     try:
-        out = bp.output.GetGlobalOut()
+        out = GetGlobalOut()
 
         # Load the python modules
         #             supermodule      module name      key
@@ -24,34 +29,30 @@ def Run(mm):
         mm.Print(out)
         mm.SanityCheck()
   
-        atoms = [ bp.system.CreateAtom(0, [ 0.000000000000, -0.143225816552,  0.000000000000], 8),
-                  bp.system.CreateAtom(1, [ 1.638036840407,  1.136548822547, -0.000000000000], 1),
-                  bp.system.CreateAtom(2, [-1.638036840407,  1.136548822547, -0.000000000000], 1)
-                ]  
-
-        molu = bp.system.AtomSetUniverse()
+        atoms = list(water)
+        u = AtomSetUniverse()
         for a in atoms:
-            molu.append(a)
+            u.Insert(a)
+        s = System(u, True)
 
-        mol = bp.system.System(molu, True)
-        mol = bp.system.ApplySingleBasis(bp.system.ShellType.Gaussian, "primary", "sto-3g", mol)
-        bs = mol.GetBasisSet("primary")
-        bs.Print()
-  
-  
+        s = ApplySingleBasis(ShellType.Gaussian,"Primary","sto-3g",s)
+
          
         eri = mm.GetModule("ERI", 0)
-        ubs = bp.datastore.MakeUIDPointer(bs)
-        eri.SetBases(4, ubs, ubs, ubs, ubs)
+        eri.EnableDebug(True)
+        iwfn = Wavefunction()
+        iwfn.system = s
+        eri.SetInitialWfn(iwfn)
+        eri.SetBases("Primary", "Primary", "Primary", "Primary")
         n = eri.Calculate(1, 0, 0, 0, 0)
-        bp.output.GlobalOutput("Calculated {} integrals\n".format(n))
-        bp.output.GlobalOutput(str(eri.GetBuf()))
-        bp.output.GlobalOutput("\n")
+        GlobalOutput("Calculated {} integrals\n".format(n))
+        GlobalOutput(str(eri.GetBuf()))
+        GlobalOutput("\n")
       
 
 
     except Exception as e:
-      bp.output.GlobalOutput("Caught exception in main handler\n")
+      GlobalOutput("Caught exception in main handler\n")
       traceback.print_exc()
 
 
