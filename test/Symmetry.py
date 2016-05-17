6e8a69eee15bb0a25805b541d2f8696bb07a87aa
Ryan Richard <ryanmrichard1@gmail.com>
2016-05-17 10:16:57 -0400

    symmetry tests

--- test/Symmetry.py
+++ test/Symmetry.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import copy
 
 # Add the bpmodule path
 thispath = os.path.dirname(os.path.realpath(__file__))
@@ -11,24 +12,111 @@ sys.path.insert(0, psrpath)
 #from StandardModules import *
 from helper.Molecules import *
 
-def TestPG(tester,Sym,PG):
-    g=lambda x: x.SSymbol==Sym
-    tester.Test(Sym,True,g,PG)
+def RightEs(PG,Elems):
+    Es=copy.deepcopy(Elems[PG.SSymbol])
+    for i in PG.Elems:
+        if Es.count(i.SSymbol)>0:
+            Es.remove(i.SSymbol)
+        else:
+            return False
+    return True if len(Es)==0 else False
+
+def RightOrder(PG,Elems):
+    if PG.Finite():
+        return PG.Order()==len(Elems[PG.SSymbol])
+    return True
+
+def TestPG(tester,IPG,PG,Elems):
+    """
+    This function first tests if our idealized point group, IPG,
+    has the correct elements by comparing to Elems[IPG.SSymbol].
+    
+    Next, it determines if our non idealized point group has the
+    correct symbol.
+    
+    Finally, we check if the non idealized point group has the righ
+    elements
+    """
+    SS=IPG.SSymbol
+    RightEs(IPG,Elems)
+    Name="Idealized "+SS
+    tester.Test(Name+" has correct elements",True,RightEs,IPG,Elems)
+    tester.Test(Name+" has correct order",True,RightOrder,IPG,Elems)
+    Name="Computed "+SS
+    tester.Test(Name+" has correct elements",True,RightEs,PG,Elems)
+    tester.Test(Name+" has correct order",True,RightOrder,PG,Elems)
+    
 
 def Run(mm):
     try:
         tester = psr.testing.Tester("Testing Symmetry")
         tester.PrintHeader()
+        
+        Cs=psr.system.Cs()
+        Ci=psr.system.Ci()
+        C2=psr.system.Cn(2)
+        C3=psr.system.Cn(3)
+        C2v=psr.system.Cnv(2)
+        C3v=psr.system.Cnv(3)
+        C4v=psr.system.Cnv(4)
+        C5v=psr.system.Cnv(5)
+        C2h=psr.system.Cnh(2)
+        C3h=psr.system.Cnh(3)
+        D2=psr.system.Dn(2)
+        D2h=psr.system.Dnh(2)
+        D3h=psr.system.Dnh(3)
+        D4h=psr.system.Dnh(4)
+        D5h=psr.system.Dnh(5)
+        D6h=psr.system.Dnh(6)
+        D2d=psr.system.Dnd(2)
+        D3d=psr.system.Dnd(3)
+        D4d=psr.system.Dnd(4)
+        D5d=psr.system.Dnd(5)
+        S4=psr.system.Sn(4)
+        Elems={"Cs":["E","s"],
+               "Ci":["E","i"],
+               "C2":["E","C_2"],
+               "C3":["E","C_3","C_3^2"],
+               "C2v":["E","C_2","s","s"],
+               "C3v":["E","C_3","C_3^2","s","s","s"],
+               "C4v":["E","C_4","C_2","C_4^3","s","s","s","s"],
+               "C5v":["E","C_5","C_5^2","C_5^3","C_5^4","s","s","s","s","s"],
+               "C2h":["E","C_2","i","s"],
+               "C3h":["E","C_3","C_3^2","s","S_3","S_3^5"],
+               "D2":["E","C_2","C_2","C_2"],
+               "D2":["E","C_2","C_2","C_2"],
+               "D2h":["E","C_2","C_2","C_2","s","s","s","i"],
+               "D3h":["E","C_3","C_3^2","s","S_3","S_3^5","s","s","s",
+                      "C_2","C_2","C_2"],
+               "D4h":["E","C_4","C_4^3","C_2","C_2","C_2","C_2","C_2","i",
+                      "S_4","S_4^3","s","s","s","s","s"],
+               "D5h":["E","C_5","C_5^4","C_5^2","C_5^3","C_2","C_2","C_2",
+                      "C_2","C_2","S_5","S_5^9","S_5^3","S_5^7",
+                       "s","s","s","s","s","s"],
+               "D6h":["E","C_6","C_6^5","C_3","C_3^2",
+                      "C_2","C_2","C_2","C_2","C_2","C_2","C_2",
+                      "i","S_3","S_3^5","S_6","S_6^5","s","s","s","s","s",
+                      "s","s"],
+               "D2d":["E","S_4","S_4^3","C_2","C_2","C_2","s","s"],
+               "D3d":["E","C_3","C_3^2","C_2","C_2","C_2","i","S_6","S_6^5",
+                      "s","s","s"],
+               "D4d":["E","S_8","S_8^3","S_8^5","S_8^7","C_4","C_4^3","C_2",
+                      "C_2","C_2","C_2","C_2","s","s","s","s"],
+               "D5d":["E","C_5","C_5^2","C_5^3","C_5^4",
+                      "C_2","C_2","C_2","C_2","C_2","i",
+                      "S_10","S_10^9","S_10^3","S_10^7","s","s","s","s","s"],
+               "S4":["E","S_4","S_4^3","C_2"]
+                    
+        }
 
-        Syms=["Cs","Ci",
-              "C2","C3",#C4,C5,C6,
-              "C2v","C3v","C4v","C5v",#C6v,
-              "C2h","C3h",#C4h,C5h,C6h
-              "D2",#D3,D4,D5,D6
-              "D2h","D3h","D4h","D5h","D6h",
-              "D2d","D3d","D4d","D5d",#D6d,
-              "S4",#S6,S8,S10,
-              #T,Td,Th,O,Oh,I,Ih,
+        Syms=[Cs,Ci,C2,C3,#C4,C5,C6,
+              C2v,C3v,C4v,C5v,#C6v,
+              C2h,C3h,#C4h,C5h,C6h
+              D2,#D3,D4,D5,D6
+              D2h,D3h,D4h,D5h,D6h,
+              D2d,D3d,D4d,D5d,#D6d,
+              S4,#S6,S8,S10,
+              "Td","Oh","Ih",#T,Th,O,I,
               "Coov","Dooh","Kh"
         ]
         Mols=[CH2BrCl,PeroxideDimer,
@@ -39,14 +127,22 @@ def Run(mm):
               Napthalene,EthaneEclip,PtCl4,C5H5Anion,Benzene,
               Allene,EthaneStag,S8,FerroceneStag,
               C8F4H4,
+              Methane,SF6,C60,
               C2HCl,C2H2,He
         ]
        
         #LoadDefaultModules(mm)
+        f=open("Test.xyz",'w')
+        f.write(str(BzCrystal.Size())+"\n\n")
+        for i in BzCrystal:
+            f.write(str(i.GetSymbol())+" "+str(i[0]/1.889725989)+" "+str(i[1]/1.889725989)+" "+str(i[2]/1.889725989)+"\n")
+        f.close()
         Symmer=psr.system.Symmetrizer()
-        
+        Symmer.GetSymmetry(BzCrystal)
+        exit()
+
         for i in range(0,len(Syms)):
-            TestPG(tester,Syms[i],Symmer.GetSymmetry(Mols[i]))
+            TestPG(tester,Syms[i],Symmer.GetSymmetry(Mols[i]),Elems)
         
      
     except Exception as e:
