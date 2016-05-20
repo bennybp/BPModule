8c556a604223ed519d05a8a19eb77549e221fe60
Ben Pritchard <ben@bennyp.org>
2016-05-20 13:14:58 -0400

    Small fix to tree printer

--- pulsar/modulemanager/ModuleTreePrinters.py
+++ pulsar/modulemanager/ModuleTreePrinters.py
@@ -15,17 +15,19 @@ def IterateModuleTree(mm):
 def PrintDotTree(mm):
     s = " digraph graphname {\n"
 
+    s += "   nodeMaster[shape=box, label=\"INPUT\"]\n"
+
     for node in IterateModuleTree(mm):
         s += "  node{}[shape=box, label=\"{} [{}]\\n{} v{}\"]\n".format(node.id,
                                                                         node.id,
                                                                         node.modulekey,
                                                                         node.minfo.name,
                                                                         node.minfo.version)
-        if node.parentid != 0:
+        if node.parentid == 0:
+            s += "  nodeMaster -> node{}\n".format(node.id)
+        else:
             s += "  node{} -> node{}\n".format(node.parentid, node.id)
 
     s += "}" # close the graph
     return s
-
-
 
