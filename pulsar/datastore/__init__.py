28c71040964b9c388ffa906d108adf675535b5d6
Ryan Richard <ryanmrichard1@gmail.com>
2016-08-16 15:50:08 -0400

    Little option facility

--- pulsar/datastore/__init__.py
+++ pulsar/datastore/__init__.py
@@ -3,3 +3,26 @@ from ..pulsar_core.datastore import *
 
 # Other files
 from .OptionValidators import *
+
+def set_options(mm,daopts):
+   """
+   A convience function for setting a lot of options at once
+   
+      Most modules have lots of options.  To clean up the input files somewhat
+      I have created this function which can be used like:
+      
+      psr.datastore.set_options(mm,{"Module1's Key":{"Option 1 for mod 1":value,
+                                                    "Option 2 for mod 1":value},
+                                    "Module2's Key":{Option 1 for mod 1":value,
+                                                    "Option 2 for mod 2":value}
+      })
+      
+      Obviously you can specify more options and more modules if you like.
+   
+      mm (psr.modulemanager.ModuleManager) : The module manager
+      daopts (dict of dict) : A nested dictionary mapping module keys to option
+                              key to option value
+   """
+   for mod in daopts:
+      for opt in daopts[mod]:
+         mm.change_option(mod,opt,daopts[mod][opt])
