c2e3e5c03d94cec35ab00fa7b99f9cdf6152436a
Ben Pritchard <ben@bennyp.org>
2016-12-17 20:49:46 -0500

    Use cmake config for pybind11

--- external/cmake/Findpybind11.cmake
+++
@@ -1,9 +0,0 @@
-#########################
-# Finds pybind11
-#########################
-
-find_path(PYBIND11_INCLUDE_DIR NAMES pybind11/pybind11.h)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(pybind11 DEFAULT_MSG
-                                  PYBIND11_INCLUDE_DIR)
