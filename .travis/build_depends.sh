38b8506b377075bb04fe61bb453e260a5de5de2f
ryanmrichard <ryanmrichard1@gmail.com>
2017-05-22 14:38:39 -0500

    Travis Try 44

---
+++ .travis/build_depends.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# This script expects two arguments compiler version and python version
+PY_VER=$1
+CXX_COMPILER=g++-$2
+g++ --version
+cd ${HOME}
+#####################################
+# Get Miniconda for python versions #
+#####################################
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+    -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda create -q -n test-environment python=${PY_VER}
+source activate test-environment
+PY_EXE=${HOME}/miniconda/envs/test-environment/bin/python
+cd ${HOME}
+###################################################
+# Build Eigen because CMake doesn't support https #
+###################################################
+wget http://bitbucket.org/eigen/eigen/get/3.3.2.tar.bz2
+tar -xf 3.3.2.tar.bz2
+cd eigen-eigen-da9b4e14c255
+cmake -H. -Bbuild -DCMAKE_CXX_COMPILER=${CXX_COMPILER} \
+                  -DCMAKE_BUILD_TYPE=Release  \
+                  -DCMAKE_INSTALL_PREFIX=${HOME} \
+cd build && make install
+cd ${HOME}
+#####################################
+#Build Pulsar-Meta for dependencies #
+#####################################
+git clone https://github.com/pulsar-chem/Pulsar-Meta.git
+cd Pulsar-Meta
+cmake -H. -Bbuild -DCMAKE_CXX_COMPILER=${CXX_COMPILER} \
+                  -DMPI_CXX_COMPILER=mpicxx -DMPI_C_COMPILER=mpicc \
+                  -DCMAKE_BUILD_TYPE=Release \
+                  -DCMAKE_INSTALL_PREFIX=${HOME} \
+                  -DCMAKE_PREFIX_PATH=${HOME} \
+                  -DPYTHON_EXECUTABLE=${PY_EXE} \
+                  -DENABLE_PULSAR=False
+cd build && make install
