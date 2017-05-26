ec5621bc702656854db889f9dbf50df7a6174828
Benjamin Pritchard <ben@bennyp.org>
2017-05-26 13:35:17 -0400

    Move lib stuff into pulsar subdir. Add examples + testing

--- pulsar/bin/pulsar_runtest.py
+++ pulsar/bin/pulsar_runtest.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-# This script runs a test for Pulsar. Its only argument it a path
-# to a script that actually does the testing.
+# This script runs a test for Pulsar.
 #
 # The purpose of this script is to import pulsar from the appropriate
 # path (relative to this file) and also do some common initialization
 # of the core. It also handles cleanup of the core as well.
+#
+# Its only required argument is a path to a script that actually does the testing.
+# Any remaining arguments are interpreted as paths to add to the search paths for
+# any modules required for the test.
 #
 # The return value is zero if everything runs ok and the test script
 # returns zero. Otherwise, if there is a problem with this script
@@ -24,25 +27,24 @@ from importlib.machinery import SourceFileLoader, ExtensionFileLoader
 # so we can import the pulsar core package
 thispath = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(thispath)
-libpath = os.path.join(parent, "lib", "pulsar", "modules")
-sys.path.insert(0, libpath)
+
+# Find the pulsar library directory
+modpath = os.path.join(parent, "lib", "pulsar", "modules")
+sys.path.insert(0, modpath)
 
 import pulsar
 
 
 # Check for the right arguments
 if len(sys.argv) < 2:
-  print("Usage: RunTest.py testfile [additional python paths]")
+  print("Usage: pulsar_runtest.py testfile [additional python paths]")
   raise RuntimeError("Error - at least one argument required")
   quit(-1)
 
-# Manually import the test file via the full path
-# Add the file's directory to the python path
-full_path = sys.argv[1]
-
 if len(sys.argv) > 2:
-    sys.path = sys.argv + sys.path
+    sys.path = sys.argv[2:] + sys.path
 
+full_path = sys.argv[1]
 if not os.path.isfile(full_path):
   raise RuntimeError("Error - path \"{}\" is not a file".format(full_path))
   quit(-2)
@@ -53,8 +55,8 @@ test_name, test_ext = os.path.splitext(test_file)
 print("---------------------------------------------------")
 print(" Test information")
 print("     full_path: {}".format(full_path))
-print("     test_path: {}".format(test_path))
-print("     test_file: {}".format(test_file))
+#print("     test_path: {}".format(test_path))
+#print("     test_file: {}".format(test_file))
 print("     test_name: {}".format(test_name))
 print("      test_ext: {}".format(test_ext))
 print("  python paths: {}".format(" ".join(sys.path)))
@@ -81,7 +83,7 @@ else:
 
 # Actually run the test, watching for exceptions
 try:
-  # Run the test in the module
+  # Run the test in the imported (python) module
   ret = m.run_test()
 except Exception as e:
   # Printing to pulsar output here so that it doesn't get
